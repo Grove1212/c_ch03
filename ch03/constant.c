a42c75a3a75c7a999515764c89aa6345ec156505
Grove1212 <stella223@korea.ac.kr>
2023-08-18 11:31:40 +0900

    초기 커밋.

---
+++ ch03/constant.c
@@ -0,0 +1,14 @@
+
+/*
+
+#include <stdio.h>
+
+int main(void)
+{
+	char grade = '\xa';
+	printf("%c", grade);
+
+	return 0;
+}
+
+*/
---
+++ ch03/variable.c
@@ -0,0 +1,17 @@
+/* Ex03_01.c */
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	int amount = 8;
+	int price = 100;
+
+	printf("수량 : %d, 가격 : %d\n", amount, price);
+	amount = 100;
+	price = 2000;
+	printf("수량 : %d, 가격 : %d\n", amount, price);
+
+	return 0;
+}
+*/
